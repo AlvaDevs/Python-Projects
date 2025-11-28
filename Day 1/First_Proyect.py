c59b4c523ec58a6f40cfa33bb0322c82657eafb3
Álvaro José Álvarez Puello <aalvarez.contact@icloud.com>
2025-11-27 23:01:06 -0500

    Initial project setup with sample Python scripts
    Added PyCharm IDE configuration files and initial Python scripts for Day 1 exercises, including basic print statements, input handling, and a simple beer name generator. Also included a sample main.py script.

---
+++ Day 1/First_Proyect.py
@@ -0,0 +1,13 @@
+# Print simple string
+print('Hello world, I say hello to you.')
+
+# Print number
+print(100 + 50)
+
+# Print strings
+print("Hey" + " " + '\'Álvaro\'')
+print("This is one line.\nAnd this is another.")
+print("\tAnd this is a third one.")
+
+# Input
+print("Tu nombre es " + (input("Tell me your first name: ")) + " " + (input("Tell me your last name: ")) + ".")
---
+++ Day 1/brewery.py
@@ -0,0 +1,3 @@
+print("Welcome to the name generator for your beer")
+print("First of all, answer these questions:")
+print("The ideal name for your beer is:\n\"" + (input("What's your favorite color?\n")) + " " + (input("What's your favorite animal?\n")) + "\"")
